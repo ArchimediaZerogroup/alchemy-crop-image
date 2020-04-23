cd7a81984dd0ece13c863f6d82802960675d39dd
Alessandro Baccanelli <alessandro.baccanelli@archimedianet.it>
2020-04-23 13:39:43 +0200

    removed isolated namespace

--- lib/alchemy/crop/image/engine.rb
+++ lib/alchemy/crop/image/engine.rb
@@ -1,10 +1,11 @@
 require "alchemy_cms"
+require "js-routes"
 require_relative "./configuration"
 module Alchemy
   module Crop
     module Image
       class Engine < ::Rails::Engine
-        isolate_namespace Alchemy::Crop::Image
+        #isolate_namespace Alchemy::Crop::Image
 
       end
     end
