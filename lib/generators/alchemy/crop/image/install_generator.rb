1e1a39ed9c8c4dfe21c66b9a360e24f49ce7f4ab
Alessandro Baccanelli <alessandro.baccanelli@archimedianet.it>
2020-04-24 17:13:20 +0200

    fix generator

--- lib/generators/alchemy/crop/image/install_generator.rb
+++ lib/generators/alchemy/crop/image/install_generator.rb
@@ -11,29 +11,16 @@ class Alchemy::Crop::Image::InstallGenerator < Rails::Generators::Base
   desc "Add required assets"
 
   def add_required_assets
-    if Gem::Version.new(Rails.version) >= Gem::Version.new("6.0.0") and Alchemy.gem_version > Gem::Version.new("4.5.0")
 
-      prepend_to_file "app/javascript/packs/alchemy/admin.js" do
-        "\nrequire('alchemy/crop/image/application')\n"
-      end
-
-      inject_into_file "vendor/assets/stylesheets/alchemy/admin/all.css", after: ' *= require alchemy/admin/alchemy-devise' do
-        "\n*= require alchemy/crop/image/application\n"
-      end
-
-    else
-      inject_into_file "vendor/assets/javascripts/alchemy/admin/all.js", after: '//= require alchemy/admin' do
-        "\n//= require alchemy/crop/image/application\n"
-      end
-
-      inject_into_file "vendor/assets/stylesheets/alchemy/admin/all.css", after: ' *= require alchemy/admin/alchemy-devise' do
-        "\n*= require alchemy/crop/image/application\n"
-      end
-
+    inject_into_file "vendor/assets/javascripts/alchemy/admin/all.js", after: '//= require alchemy/admin' do
+      "\n//= require alchemy/crop/image/application\n"
     end
+
+    inject_into_file "vendor/assets/stylesheets/alchemy/admin/all.css", after: ' *= require alchemy/admin' do
+      "\n *= require alchemy/crop/image/application\n"
+    end
+
   end
-
-
 
 
 end
