7b189485d57f65decdc0d6f02b8423ce34c5ef52
Jury Ghidinelli <juryghidinelli@gmail.com>
2021-08-31 10:39:19 +0200

    recreate routes for application routing draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
-Alchemy::Engine.routes.draw do
+Rails.application.routes.draw do
 
 
-  namespace :admin do
-    resources :pictures do
-      resource :crop, module: :pictures, only: :create
+  namespace :alchemy do
+    namespace :admin do
+      resources :pictures do
+        resource :crop, module: :pictures, only: :create
+      end
     end
   end
 
